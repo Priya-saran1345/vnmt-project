03f86b7c20dd7c25fdd17d7945c8dbb5450514ba
Abhi8099 <abhishek.sharma1@digranknow.com>
2025-03-29 12:43:34 +0530

    logo

--- src/components/projectcomponent/navbar.tsx
+++ src/components/projectcomponent/navbar.tsx
@@ -9,6 +9,7 @@ import { getMenuIcon } from "./icons"
 import Image from "next/image"
 import CountrySelector from "./countrySelect"
 import ToolbarSearch from "./search"
+import { BASE_URL } from "@/utils/api"
 // import { BASE_URL } from "@/utils/api"
 
 
@@ -318,7 +319,7 @@ export default function Navbar({ className = "" ,logo }: NavbarItemsProps) {
           <div className="w-full">
             <Link href="/">
               <Image
-                src={logo ? `${logo}` : "/images/logo.svg"}
+                src={logo ? `${BASE_URL}${logo}` : "/images/logo.svg"}
                 loading="lazy"
                 width={150}
                 height={35}
